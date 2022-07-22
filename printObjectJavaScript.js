7df1f75f4399116ec458821466a069a8ffe64ef7
pradipdhungel <66682622+pradipdhungel@users.noreply.github.com>
2022-07-21 21:02:20 -0500

    add all commit

---
+++ printObjectJavaScript.js
@@ -0,0 +1,46 @@
+// let  arrType=["str1","str2","str3","str4"];
+
+// for(let i=0;i<arrType.length;i++){
+//     // console.log(arrType[i]);
+//     if(arrType[i]=="str1")
+//         console.log(arrType[i]);
+// }
+// for(let arr of arrType){
+//     //console.log('arr');
+//     console.log(arr);
+// }
+let array2=[];
+array2[0]=1;
+array2[1]="string";
+array2[3]="ele2";
+// for(let i=0;i<array2.length;i++)
+// console.log(array2[i]);
+
+// array2.shift();
+
+// for(let i=0;i<array2.length;i++)
+// console.log(array2[i]);
+// array2.pop();
+
+// for(let i=0;i<array2.length;i++)
+// console.log(array2[i]);
+
+
+
+let ar = [2,3,4,5,6,7,8];
+// ar.pop();
+// console.log(ar);
+// ar.push(10);
+// console.log(ar);
+// console.log(ar.concat(ar));
+
+function maximum(a,b,c){
+    if(a>b && a>c)
+    console.log(a);
+    if(b>a && b>c)
+    console.log(b);
+    else 
+    console.log(c)
+
+}
+console.log(maximum(3,8,9));
